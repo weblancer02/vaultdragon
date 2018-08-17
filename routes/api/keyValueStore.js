91157b8b0d1f9c3939bd7a451b2d1e4ce811509f
Ramnel Erson Ramos <ramnel.ramos@gmail.com>
2018-08-17 12:26:41 +0800

    Added get route for retrieving value from given key

--- routes/api/keyValueStore.js
+++ routes/api/keyValueStore.js
@@ -5,8 +5,8 @@ const isEmpty = require("../../validations/is-empty");
 // Load KeyValueStore model
 const KeyValueStore = require("../../models/KeyValue");
 
-// @route   GET api/object
-// @desc    Get value from provided key
+// @route   POST api/object
+// @desc    POST Key, Value pair & Timestamp
 // @access  Public
 router.post("/", (req, res) => {
   // Perform basic validation
@@ -14,23 +14,46 @@ router.post("/", (req, res) => {
     return res.status(400).json("Key is required");
   }
 
-  KeyValueStore.findOne({ key: req.body.key }).then(key => {
-    if (key) {
-      return res
-        .status(400)
-        .json({ key: `Key ${req.body.key} already exists` });
-    } else {
-      const newKey = new KeyValueStore({
-        key: req.body.key,
-        value: req.body.value
-      });
+  KeyValueStore.findOne({ key: req.body.key })
+    .then(key => {
+      if (key) {
+        return (
+          res
+            // WIP : Change to update instead of validating existing key
+            .status(400)
+            .json({ key: `Key ${req.body.key} already exists` })
+        );
+      } else {
+        const newKey = new KeyValueStore({
+          key: req.body.key,
+          value: req.body.value
+        });
 
-      newKey
-        .save()
-        .then(newKey => res.json(newKey))
-        .catch(err => console.log(err));
-    }
-  });
+        newKey
+          .save()
+          .then(newKey => res.json(newKey))
+          .catch(err => console.log(err));
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/object/:key
+// @desc    Get value by provided key
+// @access  Public
+
+router.get("/:key", (req, res) => {
+  KeyValueStore.findOne({ key: req.params.key })
+    .then(key => {
+      if (key) {
+        res.json({ value: key.value });
+      } else {
+        return res
+          .status(400)
+          .json({ key: `No matching key found for ${req.params.key}` });
+      }
+    })
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
